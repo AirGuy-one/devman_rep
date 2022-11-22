1bafaf3e61cc8111df2c13d8cf1d05505ea46044
AirGuy-one <airguy-one@yandex.ru>
2022-11-22 16:08:19 +0300

    Add Post model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.CharField(max_length=255)
+    description_long = models.TextField()
+
+
+
+
+
