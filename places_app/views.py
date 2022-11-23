19398c77d44055940c4369b4ccb0c319cce3c8f4
AirGuy-one <airguy-one@yandex.ru>
2022-11-24 02:50:30 +0300

    Added the ability to transfer data from the admin panel to the site

--- places_app/views.py
+++ places_app/views.py
@@ -11,11 +11,6 @@ def index(request):
 
     json_convert_file = {}
 
-    json_for_html = {
-        "type": "FeatureCollection",
-        "features": []
-    }
-
     for post in posts:
         """Here we add json files to folder places"""
         json_convert_file['title'] = str(post.title)
@@ -36,30 +31,12 @@ def index(request):
         with open(f'static/places/{post.id}_json_data.json', 'w') as f:
             f.write(json_tmp)
 
-        """Here we writing json to html template"""
-        adding = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [float(post.x), float(post.y)]
-            },
-            "properties": {
-                "title": f'{post.title}',
-                "placeId": f'id_is_{post.id}',
-                "detailsUrl": "static/places/" + f"{post.id}" + "_json_data.json"
-            }
-        }
-
-        json_for_html['features'].append(adding)
-
-    json_final_file = json.dumps(json_for_html, indent=4)
-
-    print(json_final_file)
+    last = Post.objects.last()
 
     context = {
         'posts': posts,
         'images': images,
-        'json_final_file': json_final_file
+        'last': last
     }
 
     return render(request, 'index.html', context=context)
